af6ee89691782d2536de2e94fdd7a35568aa56c5
linggarelangp <linggar382@gmail.com>
2024-06-17 02:04:04 +0700

    fix: update ui room page

--- app/views/layouts/header.php
+++ app/views/layouts/header.php
@@ -7,6 +7,8 @@
     <link href="<?= BASE_URL; ?>/assets/css/style.css" rel="stylesheet">
     <link rel="icon" href="<?= BASE_URL; ?>/assets/img/logo-white.svg" type="image/x-icon" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" />
     <title><?= $data['title']; ?></title>
 </head>
 
--- app/views/rooms/index.php
+++ app/views/rooms/index.php
@@ -2,95 +2,224 @@
     <div class="facilities-hero">
         <div class="container h-100 align-content-center">
             <div class="text-center text-white">
-                <h1 class="mb-2 fw-semibold" style="letter-spacing: .1rem; padding-top: 12rem;"><u>OUR
-                        ROOMS</u></h1>
+
+                <div class="mb-2 container search-container" style="padding-top: 12rem;">
+                    <div class="input-group">
+                        <input type="date" class="form-control shadow-none rounded-start-4" placeholder="Tanggal Cek in">
+                        <input type="date" class="form-control shadow-none rounded-end-4" placeholder="Tanggal Cek Out">
+                        <div class="ms-3 input-group-append">
+                            <button class="btn btn-primary rounded-pill" type="button">
+                                <i class="fa fa-search"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <!-- <h1 class="mb-2 fw-semibold" style="letter-spacing: .1rem; padding-top: 12rem;"><u>
+                        PENCARIAN DIMARI</u></h1> -->
             </div>
         </div>
     </div>
 </div>
 
 <div id="rooms" class="container">
-    <div class="mb-3">
-        <div>
-            <label for="">Search By Date</label>
-            <input class="rounded px-3 py-1 border-1" type="date">
-        </div>
 
-        <div>
-            <label for="">Search</label>
-            <input class="rounded px-3 py-1 border-1" type="text">
-            <button class="btn btn-sm btn-outline-primary">Search</button>
-        </div>
+    <div class="container h-100">
 
     </div>
 
-    <div class="card w-100 h-100 position-relative mb-5 shadow-lg rounded-5">
-        <div class="card-body m-0 p-0">
-            <div class="row flex-wrap">
-                <div class="col-md-5">
-                    <img src="assets/img/superior.jpg" class="card-img-top object-fit-cover w-100 h-100 rounded-start-4 ">
-                </div>
-                <div class="col-sm p-3 mb-5">
-                    <h4 class="fw-light">ROOM TYPE:</h4>
-                    <h5 class="fw-bold ">SUPERIOR</h5>
-                    <p class="card-text mb-1 mt-4 fw-bold">Facilities : </p>
-                    <ul class="fw-light ">
-                        <li>Room size 32 meters</li>
-                        <li>Bathroom with Shower</li>
-                        <li>Coffe Maker</li>
-                        <li>Air Conditioner</li>
-                        <li>Television 32 inch</li>
+    <div class="row">
+        <div class="col-3 h-full rounded-4 p-3">
+            <div class="pt-3">
+                <h4 class="mb-5">Filter</h4>
+
+                <div class="mb-5">
+                    <h6 class="mb-3">Our Price</h6>
+                    <ul class="ps-3 d-grid gap-2 list-unstyled">
+                        <li><a class="text-decoration-none text-black" href="#">Cheapest</a></li>
+                        <li><a class="text-decoration-none text-black" href="#">Normal</a></li>
+                        <li><a class="text-decoration-none text-black" href="#">Expensive</a></li>
+                        <li><a class="text-decoration-none text-black" href="#">Luarang banget cik</a></li>
                     </ul>
                 </div>
-                <div class="col-sm px-5 py-3 text-end">
-                    <div class="d-none d-md-block">
-                        <h4 class="card-text fw-light">AVAILABLE</h4>
-                        <h1>20</h1>
-                        <h5>ROOM</h5>
-                    </div>
 
+                <div class="mb-5">
+                    <h6 class="mb-3">Property Facilities</h6>
+                    <div class="d-grid gap-2 list-unstyled">
+                        <div class="form-check">
+                            <input class="form-check-input shadow-none" type="checkbox" value="" id="flexCheckDefault">
+                            <label class="form-check-label" for="flexCheckDefault">
+                                TV
+                            </label>
+                        </div>
 
-                    <div class="position-absolute bottom-0 end-0 pe-4 py-3 w-50">
-                        <button type="button" class="btn btn-primary fs-5 w-50">Book Now</button>
+                        <div class="form-check">
+                            <input class="form-check-input shadow-none" type="checkbox" value="" id="flexCheckDefault2">
+                            <label class="form-check-label" for="flexCheckDefault2">
+                                Coffe Maker
+                            </label>
+                        </div>
+
+                        <div class="form-check">
+                            <input class="form-check-input shadow-none" type="checkbox" value="" id="flexCheckDefault3">
+                            <label class="form-check-label" for="flexCheckDefault3">
+                                Air Conditioner
+                            </label>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 
-    <div class="card w-100 h-100 position-relative mb-5 shadow-lg rounded-5">
-        <div class="card-body m-0 p-0">
-            <div class="row flex-wrap">
-                <div class="col-md-5">
-                    <img src="assets/img/deluxe.jpg" class="card-img-top object-fit-cover w-100 h-100 rounded-start-4 ">
-                </div>
-                <div class="col-sm p-3 mb-5">
-                    <h4 class="fw-light">ROOM TYPE:</h4>
-                    <h5 class="fw-bold ">DELUXE</h5>
-                    <p class="card-text mb-1 mt-4 fw-bold">Facilities : </p>
-                    <ul class="fw-light ">
-                        <li>Room size 32 meters</li>
-                        <li>Bathroom with Shower</li>
-                        <li>Coffe Maker</li>
-                        <li>Air Conditioner</li>
-                        <li>Television 32 inch</li>
-                    </ul>
-                </div>
-                <div class="col-sm px-5 py-3 text-end">
-                    <div class="d-none d-md-block">
-                        <h4 class="card-text fw-light">AVAILABLE</h4>
-                        <h1>20</h1>
-                        <h5>ROOM</h5>
+        <div class=" col-9">
+            <div class="container mb-4">
+                <h3 class="text-center">Our Rooms in Serenity Suites</h3>
+            </div>
+            <hr>
+            <div class="card w-100 position-relative shadow rounded-5 mb-5 border-0">
+                <div class="card-body m-0 p-0">
+                    <div class="d-flex">
+                        <div class="col-md-5 me-2">
+                            <img src="assets/img/superior.jpg" class="card-img-top object-fit-cover rounded-start-4 ">
+                        </div>
+
+                        <div class="position-relative col-md p-3">
+                            <h5>Superior Single Bed</h5>
+
+                            <p class="mt-3">Facilities</p>
+
+                            <div class="w-100">
+                                <ul class="d-flex flex-wrap pe-5 list-unstyled">
+                                    <li class="me-3 mb-2"><span class="text-black"><i class="bi bi-tv"></i></span> TV</li>
+                                    <li class="me-3 mb-2"><span class="text-black"><i class="bi bi-cup-hot"></i></span> Coffe Maker</li>
+                                    <li class="me-3 mb-2"><span class="text-black"><i class="fa fa-bath"></i></span> Bathroom</li>
+                                    <li class="me-3 mb-2"><span class="text-black"><i class="fa-solid fa-fan"></i></span> AC</li>
+                                    <li class="me-3 mb-2"><span class="text-black"><i class="fa-solid fa-bed"></i></span> Single Bed</li>
+                                    <li class="me-3 mb-2"><span class="text-black"><i class="bi bi-arrow-up-square"></i></span> Room 32 meters</li>
+                                </ul>
+                            </div>
+
+                            <div class="position-absolute bottom-0 start-0">
+                                <h5 class="ps-3 pb-3">IDR 450,000</h5>
+                            </div>
+
+                            <div class="position-absolute bottom-0 end-0 pe-4 pb-3">
+                                <button class=" btn btn-sm btn-primary rounded-pill me-1">Book Now</button>
+                                <button class=" btn btn-sm btn-success rounded-pill">Detail</button>
+                            </div>
+                        </div>
                     </div>
+                </div>
+            </div>
 
+            <div class="card w-100 position-relative shadow rounded-5 mb-5 border-0">
+                <div class="card-body m-0 p-0">
+                    <div class="d-flex">
+                        <div class="col-md-5 me-2">
+                            <img src="assets/img/superior.jpg" class="card-img-top object-fit-cover rounded-start-4 ">
+                        </div>
 
-                    <div class="position-absolute bottom-0 end-0 pe-4 py-3 w-50">
-                        <button type="button" class="btn btn-primary fs-5 w-50">Book Now</button>
+                        <div class="position-relative col-md p-3">
+                            <h5>Superior Single Bed</h5>
+
+                            <p class="mt-3">Facilities</p>
+
+                            <div class="w-100">
+                                <ul class="d-flex flex-wrap pe-5 list-unstyled">
+                                    <li class="me-3 mb-2"><span class="text-black"><i class="bi bi-tv"></i></span> TV</li>
+                                    <li class="me-3 mb-2"><span class="text-black"><i class="bi bi-cup-hot"></i></span> Coffe Maker</li>
+                                    <li class="me-3 mb-2"><span class="text-black"><i class="fa fa-bath"></i></span> Bathroom</li>
+                                    <li class="me-3 mb-2"><span class="text-black"><i class="fa-solid fa-fan"></i></span> AC</li>
+                                    <li class="me-3 mb-2"><span class="text-black"><i class="fa-solid fa-bed"></i></span> Single Bed</li>
+                                    <li class="me-3 mb-2"><span class="text-black"><i class="bi bi-arrow-up-square"></i></span> Room 32 meters</li>
+                                </ul>
+                            </div>
+
+                            <div class="position-absolute bottom-0 start-0">
+                                <h5 class="ps-3 pb-3">IDR 450,000</h5>
+                            </div>
+
+                            <div class="position-absolute bottom-0 end-0 pe-4 pb-3">
+                                <button class=" btn btn-sm btn-primary rounded-pill me-1">Book Now</button>
+                                <button class=" btn btn-sm btn-success rounded-pill">Detail</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card w-100 position-relative shadow rounded-5 mb-5 border-0">
+                <div class="card-body m-0 p-0">
+                    <div class="d-flex">
+                        <div class="col-md-5 me-2">
+                            <img src="assets/img/superior.jpg" class="card-img-top object-fit-cover rounded-start-4 ">
+                        </div>
+
+                        <div class="position-relative col-md p-3">
+                            <h5>Superior Single Bed</h5>
+
+                            <p class="mt-3">Facilities</p>
+
+                            <div class="w-100">
+                                <ul class="d-flex flex-wrap pe-5 list-unstyled">
+                                    <li class="me-3 mb-2"><span class="text-black"><i class="bi bi-tv"></i></span> TV</li>
+                                    <li class="me-3 mb-2"><span class="text-black"><i class="bi bi-cup-hot"></i></span> Coffe Maker</li>
+                                    <li class="me-3 mb-2"><span class="text-black"><i class="fa fa-bath"></i></span> Bathroom</li>
+                                    <li class="me-3 mb-2"><span class="text-black"><i class="fa-solid fa-fan"></i></span> AC</li>
+                                    <li class="me-3 mb-2"><span class="text-black"><i class="fa-solid fa-bed"></i></span> Single Bed</li>
+                                    <li class="me-3 mb-2"><span class="text-black"><i class="bi bi-arrow-up-square"></i></span> Room 32 meters</li>
+                                </ul>
+                            </div>
+
+                            <div class="position-absolute bottom-0 start-0">
+                                <h5 class="ps-3 pb-3">IDR 450,000</h5>
+                            </div>
+
+                            <div class="position-absolute bottom-0 end-0 pe-4 pb-3">
+                                <button class=" btn btn-sm btn-primary rounded-pill me-1">Book Now</button>
+                                <button class=" btn btn-sm btn-success rounded-pill">Detail</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
+
+
+        <!-- <div class="card w-100 h-100 position-relative mb-5 shadow-lg rounded-5">
+                <div class="card-body m-0 p-0">
+                    <div class="row flex-wrap">
+                        <div class="col-md-5">
+                            <img src="assets/img/deluxe.jpg" class="card-img-top object-fit-cover w-100 h-100 rounded-start-4 ">
+                        </div>
+                        <div class="col-sm p-3 mb-5">
+                            <h4 class="fw-light">ROOM TYPE:</h4>
+                            <h5 class="fw-bold ">DELUXE</h5>
+                            <p class="card-text mb-1 mt-4 fw-bold">Facilities : </p>
+                            <ul class="fw-light ">
+                                <li>Room size 32 meters</li>
+                                <li>Bathroom with Shower</li>
+                                <li>Coffe Maker</li>
+                                <li>Air Conditioner</li>
+                                <li>Television 32 inch</li>
+                            </ul>
+                        </div>
+                        <div class="col-sm px-5 py-3 text-end">
+                            <div class="d-none d-md-block">
+                                <h4 class="card-text fw-light">AVAILABLE</h4>
+                                <h1>20</h1>
+                                <h5>ROOM</h5>
+                            </div>
+
+
+                            <div class="position-absolute bottom-0 end-0 pe-4 py-3 w-50">
+                                <button type="button" class="btn btn-primary fs-5 w-50">Book Now</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div> -->
     </div>
+</div>
 </div>
 
 <script src="./assets/js/script.js"></script>
